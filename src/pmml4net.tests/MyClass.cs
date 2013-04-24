93750d81ef16d4a4c140a6faaf4de619c431843e
Damien CAROL <damien.carol@gmail.com>
2013-04-24 11:30:15 +0200

    Add Visual Studio Solution.

---
+++ src/pmml4net.tests/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Damien
+ * Date: 24/04/2013
+ * Time: 11:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace pmml4net.tests
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ src/pmml4net/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Damien
+ * Date: 24/04/2013
+ * Time: 11:27
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace pmml4net
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
