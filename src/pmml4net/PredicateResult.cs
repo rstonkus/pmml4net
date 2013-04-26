9a575725034ccd78f4407de797ed6e6d4f98456e
Damien CAROL <damien.carol@gmail.com>
2013-04-26 10:04:24 +0200

    Add LGPL v2 licence.

--- src/pmml4net/PredicateResult.cs
+++ src/pmml4net/PredicateResult.cs
@@ -1,11 +1,23 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: Damien
- * Date: 25/04/2013
- * Time: 15:39
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
+pmml4net - easy lib to read and consume tree model in PMML file
+Copyright (C) 2013  Damien Carol <damien.carol@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+Boston, MA  02110-1301, USA.
  */
+
 using System;
 
 namespace pmml4net
